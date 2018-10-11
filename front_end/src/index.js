ccc2d042978f123834f767ea0934d6fb416af4fe
João Gardenberg <joaogardenberg@gmail.com>
2018-10-10 21:08:20 -0300

    Added google-fonts, materialize-css and node-sass dependencies to the project

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import fonts from 'google-fonts';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+
+fonts.add({
+  'Open Sans': ['300', '400', '700'],
+  'Material Icons': true
+});
 
 ReactDOM.render(<App />, document.getElementById('DocAssist'));
 serviceWorker.unregister();
