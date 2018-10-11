e25231827bd6dd5ed4c2846b87f5fe6be3bc5568
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 03:32:29 -0300

    Added Sidebar and Container

--- front_end/src/components/App/App.js
+++ front_end/src/components/App/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.scss';
+import React, { Component } from 'react';
+import                           './App.scss';
+import Sidebar              from '../Sidebar/Sidebar';
+import Container            from '../Container/Container';
 
-const App = () => {
-  return (
-    <p>DocAssist</p>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Sidebar />
+        <Container />
+      </div>
+    );
+  }
 }
 
 export default App;
