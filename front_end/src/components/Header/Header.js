9026ca3ab88eb4ad8a5e6258a0e5178c06e6e4f3
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 03:52:36 -0300

    Switched to class component and added button click callbacks

--- front_end/src/components/Header/Header.js
+++ front_end/src/components/Header/Header.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import            './Header.scss';
+import React, { Component } from 'react';
+import                           './Header.scss';
 
-const Header = () => {
-  return (
-    <header className="app-header">
-      <nav className="navbar navbar-dark bg-primary">
-        <button className="btn ripple navbar-toggle" type="button">
-          <i className="fa fa-bars"></i>
-        </button>
-        <a className="navbar-brand" href="#!">
-          <h1 className="ripple">DocAssist</h1>
-        </a>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link text-white ripple" href="#!">
-              <span>Sair</span>
-              <i className="fa fa-sign-out" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <header className="app-header">
+        <nav className="navbar navbar-dark bg-primary">
+          <button
+            className="btn ripple navbar-toggle"
+            type="button"
+            onClick={ this.onNavbarToggleClick }
+          >
+            <i className="fa fa-bars"></i>
+          </button>
+          <a className="navbar-brand" href="#!" onClick={ this.onLogoClick }>
+            <h1 className="ripple">DocAssist</h1>
+          </a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-white ripple" href="#!" onClick={ this.onLogoutClick }>
+                <span>Sair</span>
+                <i className="fa fa-sign-out" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+  onNavbarToggleClick() {
+    alert('Adicionar action creator para abrir a Sidebar!');
+  }
+
+  onLogoClick() {
+    alert('Adicionar link para a página principal!');
+  }
+
+  onLogoutClick() {
+    alert('Adicionar action creator para fazer logout!');
+  }
 }
 
 export default Header;
--- front_end/src/components/Sidebar/Sidebar.js
+++ front_end/src/components/Sidebar/Sidebar.js
@@ -1,49 +1,55 @@
-import React       from 'react';
-import                  './Sidebar.scss';
+import React, { Component } from 'react';
+import                           './Sidebar.scss';
 
-const Sidebar = () => {
-  return (
-    <nav className="app-sidebar">
-      <ul className="page-links">
-        <li className="active">
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-        <li>
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-        <li>
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-        <li>
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-        <li>
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-        <li>
-          <a href="#!" className="ripple">
-            <i className="fa fa-cog" />
-            <span>Item</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+class Sidebar extends Component {
+  render() {
+    return (
+      <nav className="app-sidebar">
+        <ul className="page-links">
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 1</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 2</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 3</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 4</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 5</span>
+            </a>
+          </li>
+          <li className="active">
+            <a href="#!" className="ripple" onClick={ this.onNavbarItemClick }>
+              <i className="fa fa-cog" />
+              <span>Item 6</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  onNavbarItemClick() {
+    alert('Adicionar router e links para as páginas!');
+  }
 }
 
 export default Sidebar;
