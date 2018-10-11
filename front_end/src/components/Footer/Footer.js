1177c45f917e74f179ae67d30eefd90abf6d17e8
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 03:33:25 -0300

    Created footer files

---
+++ front_end/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import            './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>&copy; 2018 DocAssist. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
