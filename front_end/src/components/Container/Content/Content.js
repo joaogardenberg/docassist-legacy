c8c7cebefe2790e94598907b147f3788556364ea
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 03:33:17 -0300

    Created Content files

---
+++ front_end/src/components/Container/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import            './Content.scss';
+
+const Content = () => {
+  return (
+    <main className="page-content">
+      <p>Content</p>
+    </main>
+  );
+}
+
+export default Content;
