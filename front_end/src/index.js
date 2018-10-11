b4fe2794089ef5f9329ad49183a5064a00fac759
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 03:29:47 -0300

    Added bootstrap, font-awesome, jquery and popper. Also removed materialize.

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-import fonts from 'google-fonts';
-import App from './components/App/App';
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import                         './reset.css';
+import                         'bootstrap/dist/css/bootstrap.min.css';
+import                         'bootstrap/dist/js/bootstrap.min';
+import Fonts              from 'google-fonts';
+import                         'font-awesome/css/font-awesome.min.css';
+import App                from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-fonts.add({
-  'Open Sans': ['300', '400', '700'],
-  'Material Icons': true
+Fonts.add({
+  'Open Sans': ['300', '400', '700']
 });
 
-ReactDOM.render(<App />, document.getElementById('DocAssist'));
+ReactDOM.render(<App />, document.getElementById('doc-assist'));
 serviceWorker.unregister();
