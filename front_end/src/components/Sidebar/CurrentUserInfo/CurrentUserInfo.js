17f5fa03f2b672ad7440425e2a9c5d85761409db
João Gardenberg <joaogardenberg@gmail.com>
2018-10-12 00:37:09 -0300

    Added alt

--- front_end/src/components/Sidebar/CurrentUserInfo/CurrentUserInfo.js
+++ front_end/src/components/Sidebar/CurrentUserInfo/CurrentUserInfo.js
@@ -3,7 +3,7 @@ import React           from 'react';
 const CurrentUserInfo = () => {
   return (
     <a className="current-user-info ripple white-ripple" href="#!" style={{ backgroundImage: 'url(http://informationcommunicationtechnology.com/wp-content/uploads/2018/06/Blue-Sky-Background.jpg)' }}>
-      <img className="rounded-circle img-fluid" src="https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png" />
+      <img className="rounded-circle img-fluid" src="https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png" alt="Imagem do usuário"/>
       <span>Usuário atual</span>
     </a>
   );
