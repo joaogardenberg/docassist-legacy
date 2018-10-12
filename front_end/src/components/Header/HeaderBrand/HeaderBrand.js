626be69ab26345aee6014364bad6fe26691ec8df
João Gardenberg <joaogardenberg@gmail.com>
2018-10-11 23:27:07 -0300

    Componentized header brand

---
+++ front_end/src/components/Header/HeaderBrand/HeaderBrand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderBrand = () => {
+  return (
+    <a className="navbar-brand" href="#!">
+      <h1 className="ripple">DocAssist</h1>
+    </a>
+  );
+}
+
+export default HeaderBrand;
