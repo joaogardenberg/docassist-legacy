92c5711057e9cd214bd1c617eaedfb84e665070c
João Gardenberg <joaogardenberg@gmail.com>
2018-10-12 06:10:38 -0300

    Added onError to image and edit button

--- front_end/src/components/Sidebar/CurrentUserInfo/CurrentUserInfo.js
+++ front_end/src/components/Sidebar/CurrentUserInfo/CurrentUserInfo.js
@@ -1,10 +1,25 @@
-import React           from 'react';
+import React      from 'react';
+import * as Toast from '../../../common/Toast';
 
-const CurrentUserInfo = () => {
+const CurrentUserInfo = props => {
+  const { imageUrl } = props;
+  const onUserClick = () => Toast.info('Adicionar router e fazer o link.');
+  const onImageError = ({ target }) => target.src = 'https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png';
+
   return (
-    <a className="current-user-info ripple white-ripple" href="#!" style={{ backgroundImage: 'url(http://informationcommunicationtechnology.com/wp-content/uploads/2018/06/Blue-Sky-Background.jpg)' }}>
-      <img className="rounded-circle img-fluid" src="https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png" alt="Imagem do usuário"/>
-      <span>Usuário atual</span>
+    <a
+      className="current-user-info waves-effect waves-light"
+      href="#!"
+      style={{ backgroundImage: 'url(http://informationcommunicationtechnology.com/wp-content/uploads/2018/06/Blue-Sky-Background.jpg)' }}
+      onClick={ onUserClick }
+    >
+      <img
+        src={ imageUrl ? imageUrl : '' }
+        alt="Imagem do usuário"
+        onError={ onImageError }
+      />
+    <span className="name">Usuário atual</span>
+      <span className="new badge" data-badge-caption="">Editar</span>
     </a>
   );
 }
