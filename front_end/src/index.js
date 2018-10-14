6386048a27d0d979db5b4f571e460df1a1a5a530
João Gardenberg <joaogardenberg@gmail.com>
2018-10-14 10:23:08 -0300

    Added materialize to the window.M var

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk                       from 'redux-thunk';
 
 import                                       'materialize-css/dist/css/materialize.min.css';
-import                                       'materialize-css/dist/js/materialize.min.js';
 import Fonts                            from 'google-fonts';
 import                                       '@fortawesome/fontawesome-free/css/all.min.css';
 import                                       'react-toastify/dist/ReactToastify.min.css';
@@ -13,6 +12,8 @@ import                                       'react-toastify/dist/ReactToastify.
 import App                              from './components/App/App';
 import * as serviceWorker               from './serviceWorker';
 import Reducers                         from './reducers';
+
+window.M = require('materialize-css/dist/js/materialize.min.js');
 
 Fonts.add({
   'Open Sans': ['300', '400', '700']
