51b4012817a45849c0b34fb80b63da6fec7a1f07
João Gardenberg <joaogardenberg@gmail.com>
2018-10-14 13:30:13 -0300

    Removed wrapper div

--- front_end/src/components/Routes/common/Multiple/Multiple.js
+++ front_end/src/components/Routes/common/Multiple/Multiple.js
@@ -3,16 +3,10 @@ import React from 'react';
 const Multiple = props => {
   const { components, ...rest } = props;
 
-  const componentsToRender = components.map(Component => {
+  return components.map(Component => {
     const name = Component.name === 'Connect' ? Component.WrappedComponent.name : Component.name;
     return <Component key={ name } { ...rest } />
   });
-
-  return (
-    <div>
-      { componentsToRender }
-    </div>
-  );
 }
 
 export default Multiple;
