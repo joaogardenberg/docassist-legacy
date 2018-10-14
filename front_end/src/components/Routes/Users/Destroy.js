6d654e1cebb8133d19be607c0fa3dac9befddeb1
João Gardenberg <joaogardenberg@gmail.com>
2018-10-14 13:30:52 -0300

    Oops, ctrl-c ctrl-v

--- front_end/src/components/Routes/Users/Destroy.js
+++ front_end/src/components/Routes/Users/Destroy.js
@@ -1,7 +1,6 @@
 import React, { Component }   from 'react';
 import { connect as Connect } from 'react-redux';
 import { Link }               from 'react-router-dom';
-import { fetchUsers }         from '../../../actions';
 import Modal                  from '../common/Modal/Modal';
 
 class UsersDestroy extends Component {
--- front_end/src/components/Routes/Users/Show.js
+++ front_end/src/components/Routes/Users/Show.js
@@ -1,7 +1,6 @@
 import React, { Component }   from 'react';
 import { connect as Connect } from 'react-redux';
 import { Link }               from 'react-router-dom';
-import { fetchUsers }         from '../../../actions';
 import Modal                  from '../common/Modal/Modal';
 
 class UsersShow extends Component {
