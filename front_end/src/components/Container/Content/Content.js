450c7b73e42470513ba0f8388ab3b4e32ec57191
João Gardenberg <joaogardenberg@gmail.com>
2018-10-13 23:22:34 -0300

    Added page loader

--- front_end/src/components/Container/Content/Content.js
+++ front_end/src/components/Container/Content/Content.js
@@ -1,11 +1,13 @@
-import React  from 'react';
-import             './Content.scss';
-import Routes from '../../Routes/Routes';
+import React      from 'react';
+import                 './Content.scss';
+import Routes     from '../../Routes/Routes';
+import PageLoader from './PageLoader/PageLoader';
 
 const Content = () => {
   return (
     <main className="page-content">
       <Routes />
+      <PageLoader />
     </main>
   );
 }
