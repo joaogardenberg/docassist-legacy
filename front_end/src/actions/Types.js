9b59e74397753a8fc9af47cf6824f825680afdfd
João Gardenberg <joaogardenberg@gmail.com>
2018-10-13 23:20:40 -0300

    Added more types

--- front_end/src/actions/Types.js
+++ front_end/src/actions/Types.js
@@ -1,3 +1,11 @@
 /* Sidebar */
-export const OPEN_SIDEBAR = 'OPEN_SIDEBAR';
-export const CLOSE_SIDEBAR = 'CLOSE_SIDEBAR';
+export const OPEN_SIDEBAR          = 'OPEN_SIDEBAR';
+export const CLOSE_SIDEBAR         = 'CLOSE_SIDEBAR';
+
+/* Loader */
+export const OPEN_LOADER           = 'OPEN_LOADER';
+export const CLOSE_LOADER          = 'CLOSE_LOADER';
+
+/* Users */
+export const FETCH_USERS           = 'FETCH_USERS';
+export const UNABLE_TO_FETCH_USERS = 'UNABLE_TO_FETCH_USERS';
