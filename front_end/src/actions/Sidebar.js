24a8e4c9f7ebc9ee18f4a9a8a0a354e730dc98cf
João Gardenberg <joaogardenberg@gmail.com>
2018-10-13 23:20:30 -0300

    Made it better

--- front_end/src/actions/Sidebar.js
+++ front_end/src/actions/Sidebar.js
@@ -4,9 +4,9 @@ import {
 } from './Types';
 
 export function openSidebar() {
-  return dispatch => dispatch({ type: OPEN_SIDEBAR });
+  return { type: OPEN_SIDEBAR };
 }
 
 export function closeSidebar() {
-  return dispatch => dispatch({ type: CLOSE_SIDEBAR });
+  return { type: CLOSE_SIDEBAR };
 }
