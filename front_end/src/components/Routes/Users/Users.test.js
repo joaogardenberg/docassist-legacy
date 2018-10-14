fb296f154958fc5a395f40be15181ef81c0671e0
João Gardenberg <joaogardenberg@gmail.com>
2018-10-14 10:21:25 -0300

    Made test better

--- front_end/src/components/Routes/Users/Users.test.js
+++ front_end/src/components/Routes/Users/Users.test.js
@@ -1,9 +1,21 @@
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import Users    from './Users';
+import React        from 'react';
+import ReactDOM     from 'react-dom';
+import UsersIndex   from './Index';
+import UsersNew     from './New';
+import UsersEdit    from './Edit';
+import UsersDestroy from './Destroy';
+import UsersShow    from './Show';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Users />, div);
+  ReactDOM.render((
+    <div>
+      <UsersIndex />
+      <UsersNew />
+      <UsersEdit />
+      <UsersDestroy />
+      <UsersShow />
+    </div>
+  ), div);
   ReactDOM.unmountComponentAtNode(div);
 });
