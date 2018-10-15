c7014ee78a6fc68d143693939b5f89a99736ce53
João Gardenberg <joaogardenberg@gmail.com>
2018-10-15 13:42:10 -0300

    Added search bar

---
+++ front_end/src/components/Routes/common/SearchBar/SearchBar.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import posed                from 'react-pose';
+
+const INITIAL_STATE = {
+  active: false,
+  value: ''
+};
+
+const Container = posed.div({
+  closed: { width: 'calc(1.5em + 40px)' },
+  open: { width: '100%' }
+});
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return (
+      <Container
+        className={ `search-bar bottom-shadow${active ? ' active' : ''}` }
+        data-position="top"
+        data-tooltip="Pesquisar usuários"
+        pose={ active ? 'open' : 'closed' }
+      >
+        <label
+          className="search-label"
+          htmlFor="search"
+          onClick={ this.onCloseButtonClick.bind(this) }
+        >
+          <i className="fas fa-search" />
+        </label>
+        <input
+          id="search"
+          className="search-input"
+          type="search"
+          value={ this.state.value }
+          onChange={ this.onSearchBarChange.bind(this) }
+          onFocus={ this.onSearchBarFocus.bind(this) }
+          onBlur={ this.onSearchBarBlur.bind(this) }
+        />
+        <i
+          className="close-button fas fa-times"
+          onClick={ this.onCloseButtonClick.bind(this) }
+        />
+      </Container>
+    );
+  }
+
+  componentDidUpdate() {
+    this.props.callback(this.state.value);
+  }
+
+  onSearchBarChange({ target: { value } }) {
+    this.setState({ value });
+  }
+
+  onSearchBarFocus() {
+    if (!this.state.active) {
+      this.setState({ active: true });
+    }
+  }
+
+  onSearchBarBlur() {
+    if (this.state.value === '') {
+      this.setState({ active: false });
+    }
+  }
+
+  onCloseButtonClick() {
+    this.setState(INITIAL_STATE);
+  }
+}
+
+export default SearchBar;
