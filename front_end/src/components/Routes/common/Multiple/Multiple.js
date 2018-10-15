4015ac5002a48ce4b2f64602c698634488f6c655
João Gardenberg <joaogardenberg@gmail.com>
2018-10-15 13:43:13 -0300

    Added temporary todo list

--- front_end/src/components/Routes/common/Multiple/Multiple.js
+++ front_end/src/components/Routes/common/Multiple/Multiple.js
@@ -3,10 +3,22 @@ import React from 'react';
 const Multiple = props => {
   const { components, ...rest } = props;
 
-  return components.map(Component => {
+  const temp = components.map(Component => {
     const name = Component.name === 'Connect' ? Component.WrappedComponent.name : Component.name;
     return <Component key={ name } { ...rest } />
   });
+
+  temp.push(
+    <div key="temp">
+      <p>Todo Usuários:</p>
+      <p>- Fazer transição de abertura do modal;</p>
+      <p>- Construir conteúdos dos modais;</p>
+      <p>- Fazer swipe para abrir/fechar sidebar;</p>
+      <p>- Fazer funcionar como se tivesse back end, reiniciando tudo quando der refresh.</p>
+    </div>
+  );
+
+  return temp;
 }
 
 export default Multiple;
