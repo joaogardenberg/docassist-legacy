09b5a0ddf07073abcc53a0240e9d6e3679f26491
João Gardenberg <joaogardenberg@gmail.com>
2018-10-15 13:41:30 -0300

    Now dynamic and randomic

--- front_end/src/services/apis/Users.js
+++ front_end/src/services/apis/Users.js
@@ -1,18 +1,14 @@
 export function fetchUsers() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([
-        { id: 1, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 2, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 3, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 4, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 5, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 6, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 7, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 8, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 9, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' },
-        { id: 10, name: 'João Lucas Gardenberg', username: 'joaogardenberg', type: 'Médico' }
-      ]);
+      const objects = [];
+      const random = Math.floor(Math.random() * 71) + 30;
+
+      for (let i = 0; i < random; i++) {
+        objects.push({ id: i, name: `Johnny ${i}`, username: `johnny${i}`, type: 'Médico' });
+      }
+
+      resolve(objects);
     }, 100 + Math.floor(Math.random() * 2001));
   });
 }
