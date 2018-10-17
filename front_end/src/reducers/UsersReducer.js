4d758b165192c18022e703dcb0097723ba3fe81f
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:25:13 -0300

    Added the rest of the User stuff

--- front_end/src/reducers/UsersReducer.js
+++ front_end/src/reducers/UsersReducer.js
@@ -1,6 +1,12 @@
 import {
   FETCH_USERS,
-  UNABLE_TO_FETCH_USERS
+  CREATE_USER,
+  UPDATE_USER,
+  DESTROY_USER,
+  UNABLE_TO_FETCH_USERS,
+  UNABLE_TO_CREATE_USER,
+  UNABLE_TO_UPDATE_USER,
+  UNABLE_TO_DESTROY_USER
 } from '../actions/Types';
 
 import _          from 'lodash';
@@ -11,9 +17,23 @@ const INITIAL_STATE = {};
 function UsersReducer(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_USERS:
-      return { ...state, users: _.mapKeys(action.payload, 'id') };
+      return _.mapKeys(action.payload, 'id');
+    case CREATE_USER:
+    case UPDATE_USER:
+      return { ...state, [action.payload.id]: action.payload };
+    case DESTROY_USER:
+      return _.omit(state, `${action.payload}`);
     case UNABLE_TO_FETCH_USERS:
       Toast.error('Ocorreu um erro ao carregar os usuários. Tente recarregar a página.', 0);
+      return { ...state };
+    case UNABLE_TO_CREATE_USER:
+      Toast.error('Ocorreu um erro ao criar o usuário.', 0);
+      return { ...state };
+    case UNABLE_TO_UPDATE_USER:
+      Toast.error('Ocorreu um erro ao atualizar o usuário.', 0);
+      return { ...state };
+    case UNABLE_TO_DESTROY_USER:
+      Toast.error('Ocorreu um erro ao remover o usuário. Tente novamente mais tarde.', 0);
       return { ...state };
     default:
       return { ...state };
