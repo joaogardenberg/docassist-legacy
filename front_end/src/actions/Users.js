aff26e6c252e6d9f319999e3818124384f834ad4
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:21:49 -0300

    Added the rest of the user action creators

--- front_end/src/actions/Users.js
+++ front_end/src/actions/Users.js
@@ -1,14 +1,22 @@
 import {
   FETCH_USERS,
-  UNABLE_TO_FETCH_USERS
+  CREATE_USER,
+  UPDATE_USER,
+  DESTROY_USER,
+  UNABLE_TO_FETCH_USERS,
+  UNABLE_TO_CREATE_USER,
+  UNABLE_TO_UPDATE_USER,
+  UNABLE_TO_DESTROY_USER
 } from './Types';
 
 import * as Api from '../services/apis/Users';
 
-import { closeLoader } from '.';
+import { openLoader, closeLoader } from '.';
 
 export function fetchUsers() {
   return dispatch => {
+    dispatch(openLoader());
+
     Api.fetchUsers()
     .then(data => {
       dispatch(closeLoader());
@@ -22,8 +30,81 @@ export function fetchUsers() {
       dispatch(closeLoader());
 
       return dispatch({
-        type: UNABLE_TO_FETCH_USERS
+        type: UNABLE_TO_FETCH_USERS,
+        payload: error
       });
     });
   };
+}
+
+export function createUser(attributes) {
+  return dispatch => {
+    dispatch(openLoader());
+
+    Api.createUser(attributes)
+    .then(data => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: CREATE_USER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: UNABLE_TO_CREATE_USER,
+        payload: error
+      });
+    });
+  }
+}
+
+export function updateUser(attributes) {
+  return dispatch => {
+    dispatch(openLoader());
+
+    Api.updateUser(attributes)
+    .then(data => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: UPDATE_USER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: UNABLE_TO_UPDATE_USER,
+        payload: error
+      });
+    });
+  }
+}
+
+export function destroyUser(id) {
+  return dispatch => {
+    dispatch(openLoader());
+
+    Api.destroyUser(id)
+    .then(data => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: DESTROY_USER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch(closeLoader());
+
+      return dispatch({
+        type: UNABLE_TO_DESTROY_USER,
+        payload: error
+      });
+    });
+  }
 }
