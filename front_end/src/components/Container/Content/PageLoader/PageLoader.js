9d1a8fc2d34608a6a81e210d293be56307c6486e
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:22:18 -0300

    Fixed state

--- front_end/src/components/Container/Content/PageLoader/PageLoader.js
+++ front_end/src/components/Container/Content/PageLoader/PageLoader.js
@@ -1,10 +1,10 @@
 import React                       from 'react';
 import { connect as Connect }      from 'react-redux';
 import { openLoader, closeLoader } from '../../../../actions';
-import                             './PageLoader.scss';
+import                                  './PageLoader.scss';
 
 const PageLoader = props => {
-  const { active } = props.loader;
+  const { active } = props;
 
   return (
     <div className={`page-loader ${active ? 'active' : 'inactive'}`}>
@@ -13,9 +13,11 @@ const PageLoader = props => {
           <div className="spinner-layer spinner-blue-only">
             <div className="circle-clipper left">
               <div className="circle" />
-            </div><div className="gap-patch">
+            </div>
+            <div className="gap-patch">
               <div className="circle" />
-            </div><div className="circle-clipper right">
+            </div>
+            <div className="circle-clipper right">
               <div className="circle" />
             </div>
           </div>
@@ -26,7 +28,7 @@ const PageLoader = props => {
 }
 
 function mapStateToProps({ loader }) {
-  return { loader };
+  return loader;
 }
 
 export default Connect(mapStateToProps, { openLoader, closeLoader })(PageLoader);
