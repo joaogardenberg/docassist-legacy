74317e757a8d2fab36e5afc0261618a003145ffb
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:21:30 -0300

    Added a bunch of types

--- front_end/src/actions/Types.js
+++ front_end/src/actions/Types.js
@@ -1,15 +1,25 @@
 /* Body */
-export const REMOVE_BODY_OVERFLOW  = 'REMOVE_BODY_OVERFLOW'
-export const ADD_BODY_OVERFLOW     = 'ADD_BODY_OVERFLOW'
+export const REMOVE_BODY_OVERFLOW   = 'REMOVE_BODY_OVERFLOW'
+export const ADD_BODY_OVERFLOW      = 'ADD_BODY_OVERFLOW'
 
 /* Sidebar */
-export const OPEN_SIDEBAR          = 'OPEN_SIDEBAR';
-export const CLOSE_SIDEBAR         = 'CLOSE_SIDEBAR';
+export const OPEN_SIDEBAR           = 'OPEN_SIDEBAR';
+export const CLOSE_SIDEBAR          = 'CLOSE_SIDEBAR';
 
 /* Loader */
-export const OPEN_LOADER           = 'OPEN_LOADER';
-export const CLOSE_LOADER          = 'CLOSE_LOADER';
+export const OPEN_LOADER            = 'OPEN_LOADER';
+export const CLOSE_LOADER           = 'CLOSE_LOADER';
+
+/* Page Modal */
+export const PAGE_MODAL_OPENED      = 'PAGE_MODAL_OPENED';
+export const PAGE_MODAL_CLOSED      = 'PAGE_MODAL_CLOSED';
 
 /* Users */
-export const FETCH_USERS           = 'FETCH_USERS';
-export const UNABLE_TO_FETCH_USERS = 'UNABLE_TO_FETCH_USERS';
+export const FETCH_USERS            = 'FETCH_USERS';
+export const CREATE_USER            = 'CREATE_USER';
+export const UPDATE_USER            = 'UPDATE_USER';
+export const DESTROY_USER           = 'DESTROY_USER';
+export const UNABLE_TO_FETCH_USERS  = 'UNABLE_TO_FETCH_USERS';
+export const UNABLE_TO_CREATE_USER  = 'UNABLE_TO_CREATE_USER';
+export const UNABLE_TO_UPDATE_USER  = 'UNABLE_TO_UPDATE_USER';
+export const UNABLE_TO_DESTROY_USER = 'UNABLE_TO_DESTROY_USER';
