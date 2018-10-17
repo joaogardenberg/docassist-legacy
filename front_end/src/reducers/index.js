8445c46ddf5bf1f2b6ef4add1d97b9c141577464
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:25:31 -0300

    Added formReducer and PageModal

--- front_end/src/reducers/index.js
+++ front_end/src/reducers/index.js
@@ -1,14 +1,18 @@
-import { combineReducers } from 'redux';
-import SidebarReducer      from './SidebarReducer';
-import BodyReducer         from './BodyReducer';
-import LoaderReducer       from './LoaderReducer';
-import UsersReducer        from './UsersReducer';
+import { combineReducers }        from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import SidebarReducer             from './SidebarReducer';
+import BodyReducer                from './BodyReducer';
+import LoaderReducer              from './LoaderReducer';
+import PageModalReducer           from './PageModalReducer';
+import UsersReducer               from './UsersReducer';
 
 const RootReducer = combineReducers({
   body: BodyReducer,
   sidebar: SidebarReducer,
   loader: LoaderReducer,
+  pageModal: PageModalReducer,
   users: UsersReducer,
+  form: formReducer
 });
 
 export default RootReducer;
