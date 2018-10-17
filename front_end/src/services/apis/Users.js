c1a8c7ad454b2987e12ac7f4b9af0b99d089006d
João Gardenberg <joaogardenberg@gmail.com>
2018-10-17 19:25:44 -0300

    Made all of the mocks

--- front_end/src/services/apis/Users.js
+++ front_end/src/services/apis/Users.js
@@ -2,13 +2,44 @@ export function fetchUsers() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const objects = [];
-      const random = Math.floor(Math.random() * 71) + 30;
+      const random = 5;
 
       for (let i = 0; i < random; i++) {
-        objects.push({ id: i, name: `João Lucas Gardenberg ${i}`, username: `johnny${i}`, type: 'Médico' });
+        objects.push({
+          id: `${new Date().getTime()}${Math.floor(Math.random() * 10001)}`,
+          name: `João Lucas Gardenberg ${i}`,
+          username: `johnny${i}`,
+          type: 'Médico(a)'
+        });
       }
 
       resolve(objects);
+    }, 100 + Math.floor(Math.random() * 2001));
+  });
+}
+
+export function createUser(attributes) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dup = Object.assign({}, attributes);
+      dup.id = `${new Date().getTime()}${Math.floor(Math.random() * 10001)}`;
+      resolve(dup);
+    });
+  }, 100 + Math.floor(Math.random() * 2001));
+}
+
+export function updateUser(attributes) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(attributes);
+    }, 100 + Math.floor(Math.random() * 2001));
+  });
+}
+
+export function destroyUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(id);
     }, 100 + Math.floor(Math.random() * 2001));
   });
 }
