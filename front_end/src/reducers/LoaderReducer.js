fccf6beca88b24f26940b335503bcef10bc1db19
João Gardenberg <joaogardenberg@gmail.com>
2018-11-02 06:43:06 -0300

    Now counting wants

--- front_end/src/reducers/LoaderReducer.js
+++ front_end/src/reducers/LoaderReducer.js
@@ -4,15 +4,22 @@ import {
 } from '../actions/Types';
 
 const INITIAL_STATE = {
+  wants: 0,
   active: false
 };
 
 function LoaderReducer(state = INITIAL_STATE, action) {
   switch(action.type) {
     case OPEN_LOADER:
-      return { active: true };
+      return { active: true, wants: state.wants + 1 };
     case CLOSE_LOADER:
-      return { active: false };
+      let active = true;
+
+      if (state.wants - 1 <= 0) {
+        active = false;
+      }
+
+      return { active, wants: state.wants - 1 }
     default:
       return state;
   }
