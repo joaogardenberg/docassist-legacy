540576da41617c779f7ec509ae4eb27dd98613bd
João Gardenberg <joaogardenberg@gmail.com>
2018-11-02 09:01:53 -0300

    Changed shouldClose to shouldGoBack, fixed some stuff, added link to userName, added user to state

--- front_end/src/components/Routes/Users/Destroy.js
+++ front_end/src/components/Routes/Users/Destroy.js
@@ -4,15 +4,18 @@ import { Link, withRouter }   from 'react-router-dom';
 import PageModal              from '../common/PageModal/PageModal';
 import { destroyUser }        from '../../../actions';
 
+const INITIAL_STATE = {
+  shouldGoBack: false,
+  user: {}
+}
+
 class UsersDestroy extends Component {
   render() {
-    const { users, match: { params: { id } } } = this.props;
+    const { shouldGoBack, user } = this.state;
 
-    if (!users) {
+    if (Object.keys(user).length < 1) {
       return null;
     }
-
-    const user = users[id];
 
     return (
       <PageModal
@@ -20,8 +23,18 @@ class UsersDestroy extends Component {
         iconClass="fas fa-trash-alt"
         footer={ this.modalFooter() }
         backTo="/usuarios"
+        shouldGoBack={ shouldGoBack }
       >
-        <p>Você tem certeza de que deseja remover o usuário "{ user.name }"?</p>
+        <p>
+          Você tem certeza de que deseja remover o usuário&nbsp;
+          <Link
+            to={ `/usuarios/${user.id}` }
+            className="link"
+          >
+            { user.name }
+          </Link>
+          ?
+        </p>
       </PageModal>
     );
   }
@@ -36,18 +49,63 @@ class UsersDestroy extends Component {
           <i className="fas fa-trash-alt left" />
           Remover
         </button>
-        <Link to="/usuarios" className="btn-flat waves-effect">
+        <button
+          className="btn-flat waves-effect"
+          onClick={ this.onBackButtonClick.bind(this) }
+        >
           <i className="fas fa-arrow-left left" />
           Voltar
-        </Link>
+        </button>
       </div>
     );
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+    this.userLoaded = false;
+  }
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate() {
+    if (this.state.shouldGoBack) {
+      this.setState({ shouldGoBack: false });
+    }
+
+    this.loadUser();
   }
 
   onDestroyButtonClick() {
     const { destroyUser, history, match: { params: { id } } } = this.props;
     destroyUser(id);
     history.push('/usuarios');
+  }
+
+  onBackButtonClick() {
+    this.setState({ shouldGoBack: true });
+  }
+
+  loadUser() {
+    const { users, match: { params: { id } }, history } = this.props;
+
+    if (id !== this.lastId) {
+      this.userLoaded = false;
+      this.lastId = id;
+    }
+
+    if (!this.userLoaded && Object.keys(users).length > 0) {
+      if (users[id]) {
+        this.setState({ user: users[id] });
+      } else {
+        history.push('/usuarios');
+      }
+
+      this.userLoaded = true;
+    }
   }
 }
 
