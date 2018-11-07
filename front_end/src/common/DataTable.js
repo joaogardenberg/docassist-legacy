71641b4b043a81e17c17d622ea0bb7b5b98a6e58
João Gardenberg <joaogardenberg@gmail.com>
2018-11-07 05:16:26 -0200

    Indentation

--- front_end/src/common/DataTable.js
+++ front_end/src/common/DataTable.js
@@ -1,25 +1,25 @@
 export function language() {
   return {
-    decimal: ',',
-    emptyTable: 'Não há nenhum registro',
-    info: 'Mostrando _START_ - _END_ de _TOTAL_',
-    infoEmpty: 'Mostrando 0',
-    infoFiltered: '(filtrados de um total de _MAX_)',
-    infoPostFix: '',
-    thousands: '.',
-    lengthMenu: '_MENU_ por página',
-    loadingRecords: 'Carregando...',
-    processing: 'Processando...',
-    search: 'Pesquisar:',
-    zeroRecords: 'Não foi encontrado nenhum registro',
+    decimal:          ',',
+    emptyTable:       'Não há nenhum registro',
+    info:             'Mostrando _START_ - _END_ de _TOTAL_',
+    infoEmpty:        'Mostrando 0',
+    infoFiltered:     '(filtrados de um total de _MAX_)',
+    infoPostFix:      '',
+    thousands:        '.',
+    lengthMenu:       '_MENU_ por página',
+    loadingRecords:   'Carregando...',
+    processing:       'Processando...',
+    search:           'Pesquisar:',
+    zeroRecords:      'Não foi encontrado nenhum registro',
     paginate: {
-      first: 'Primeira',
-      last: 'Última',
-      next: 'Próxima',
-      previous: 'Anterior'
+      first:          'Primeira',
+      last:           'Última',
+      next:           'Próxima',
+      previous:       'Anterior'
     },
     aria: {
-      sortAscending: ': em ordem crescente',
+      sortAscending:  ': em ordem crescente',
       sortDescending: ': em ordem decrescente'
     }
   };
