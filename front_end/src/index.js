9016917daf6fe84989883227f260e172bbbcb699
João Gardenberg <joaogardenberg@gmail.com>
2018-11-07 05:16:32 -0200

    Added comments

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -13,16 +13,26 @@ import App                              from './components/App/App';
 import * as serviceWorker               from './serviceWorker';
 import Reducers                         from './reducers';
 
+// Remove alert() function
 window.alert = () => {};
+
+// Add Materialize to the variable window.M
 window.M = require('materialize-css');
+
+// Add jQuery to the variables window.jquery, window.jQuery and window.$
 window.jquery = window.jQuery = window.$ = require('jquery');
+
+// Add DataTables to the variable window.$.DataTable
 window.$.DataTable = require('datatables');
+
+// DataTables changes
 // window.$.fn.DataTable.ext.pager.numbers_length = 7;
 window.$.fn.DataTable.ext.classes = {
   ...window.$.fn.DataTable.ext.classes,
   sPageButton: 'btn-flat waves-effect'
 }
 
+// Add Open Sans font
 Fonts.add({
   'Open Sans': ['300', '400', '700']
 });
