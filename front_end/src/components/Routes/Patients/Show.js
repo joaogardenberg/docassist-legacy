6b7ce5e57d74197df1bf89ec36f9963de72ad0ce
João Gardenberg <joaogardenberg@gmail.com>
2018-11-10 02:49:52 -0200

    Started Patients show page

--- front_end/src/components/Routes/Patients/Show.js
+++ front_end/src/components/Routes/Patients/Show.js
@@ -1,5 +1,8 @@
 import React, { Component }   from 'react';
 import { connect as Connect } from 'react-redux';
+import Article                from '../common/Article/Article';
+import { fetchPatient }       from '../../../actions';
+import * as Patient           from '../../../constants/Patient';
 
 const INITIAL_STATE = {
   patient: {}
@@ -13,15 +16,38 @@ class PatientsShow extends Component {
       return null;
     }
 
+    const { gender, maritalStatus, nationality, nationalityOther }   = patient;
+    const { placeOfBirth, placeOfBirthOther, name, dateOfBirth }     = patient;
+    const { occupation, cpf, rg, rgIssuingAgency, imageUrl }         = patient;
+
+    const genderName = Patient.getGenderName(gender);
+    const maritalStatusName = Patient.getMaritalStatusName(maritalStatus);
+    const rgWithIssuingAgency = Patient.getRgWithIssuingAgency(rg, rgIssuingAgency);
+    const nationalityName = Patient.getNationalityName(nationality, nationalityOther);
+    const placeOfBirthName = Patient.getPlaceOfBirthName(placeOfBirth, placeOfBirthOther);
+
     return (
-      <div className="show">
-        <div className="row">
-          <div className="col l6 s12">
-            <h5>Nome</h5>
-            <p>{ patient.name }</p>
-          </div>
+      <Article
+        uniqueClass="patients-show"
+        header="Paciente"
+      >
+        <div className="show">
+          <img
+            src={ imageUrl ? imageUrl : '' }
+            alt={ `Foto de ${name}` }
+            onError={ this.onImageError }
+          />
+          <p>Nome: { name }</p>
+          <p>Gênero: { genderName }</p>
+          <p>Estado civil: { maritalStatusName }</p>
+          <p>Data de nascimento: { dateOfBirth }</p>
+          <p>Profissão: { occupation }</p>
+          <p>CPF: { cpf }</p>
+          <p>RG: { rgWithIssuingAgency }</p>
+          <p>Nacionalidade: { nationalityName }</p>
+          <p>Naturalidade: { placeOfBirthName }</p>
         </div>
-      </div>
+      </Article>
     );
   }
 
@@ -30,52 +56,30 @@ class PatientsShow extends Component {
 
     this.state = INITIAL_STATE;
     this.patientLoaded = false;
-    this.lastId = props.match.params.id;
-    this.editTimeout = null;
   }
 
   componentDidMount() {
-    this.loadPatient();
+    const { patients, fetchPatient, match: { params: { id } } } = this.props;
+
+    if (patients[id] && Object.keys(patients[id]).length > 0) {
+      this.setState({ patient: patients[id] });
+      this.patientLoaded = true;
+    } else {
+      fetchPatient(id);
+    }
   }
 
   componentDidUpdate() {
-    this.loadPatient();
-  }
+    const { patients, match: { params: { id } } } = this.props;
 
-  componentWillUnmount() {
-    clearTimeout(this.editTimeout);
-  }
-
-  onEditButtonClick() {
-    const { history } = this.props;
-    const { patient } = this.state;
-
-    if (Object.keys(patient).length > 0) {
-      this.editTimeout = setTimeout(() => history.push(`/pacientes/${patient.id}/editar`), 200);
-    }
-  }
-
-  onBackButtonClick() {
-    this.props.history.goBack();
-  }
-
-  loadPatient() {
-    const { patients, match: { params: { id } }, history } = this.props;
-
-    if (id !== this.lastId) {
-      this.patientLoaded = false;
-      this.lastId = id;
-    }
-
-    if (!this.patientLoaded && Object.keys(patients).length > 0) {
-      if (patients[id]) {
-        this.setState({ patient: patients[id] });
-      } else {
-        history.push('/pacientes');
-      }
-
+    if (!this.patientLoaded && patients[id] && Object.keys(patients[id]).length > 0) {
+      this.setState({ patient: patients[id] });
       this.patientLoaded = true;
     }
+  }
+
+  onImageError({ target }) {
+    target.src = 'https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png';
   }
 }
 
@@ -84,7 +88,8 @@ function mapStateToProps({ patients }) {
 }
 
 PatientsShow = Connect(
-  mapStateToProps
+  mapStateToProps,
+  { fetchPatient }
 )(PatientsShow);
 
 export default PatientsShow;
