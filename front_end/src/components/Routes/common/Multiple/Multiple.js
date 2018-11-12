b9240bcb3745b06f05ab77a14684c27c266e2871
João Gardenberg <joaogardenberg@gmail.com>
2018-11-11 23:26:46 -0200

    Done some TODOs

--- front_end/src/components/Routes/common/Multiple/Multiple.js
+++ front_end/src/components/Routes/common/Multiple/Multiple.js
@@ -11,11 +11,8 @@ const Multiple = props => {
   temp.push(
     <div key="temp" className="todo" onClick={ ({ target }) => target.closest('.todo').style = 'display: none' }>
       <h5>TODO</h5>
-      <p>Ajeitar modal no celular (ajeitou sozinho?)</p>
       <p>Terminar formulário do paciente</p>
       <p>Fazer página do paciente</p>
-      <p>Abrir datepicker só com botão (pra ser opcional)</p>
-      <p>Ajeitar scroll do modal no mobile</p>
     </div>
   );
 
